959cb4f83c7d6031e9b1a591a058cd716847ccc5
Lakshmi Addepalli <lakshi.ana@gmail.com>
2019-04-15 04:44:45 -0400

    Create duplicateSubtree.java

---
+++ Uber/duplicateSubtree.java
@@ -0,0 +1,19 @@
+class Solution {
+    Map<String, Integer> count;
+    List<TreeNode> ans;
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        count = new HashMap();
+        ans = new ArrayList();
+        collect(root);
+        return ans;
+    }
+
+    public String collect(TreeNode node) {
+        if (node == null) return "#";
+        String serial = node.val + "," + collect(node.left) + "," + collect(node.right);
+        count.put(serial, count.getOrDefault(serial, 0) + 1);
+        if (count.get(serial) == 2)
+            ans.add(node);
+        return serial; 
+    }
+}
