cd725b5eaf0c26f9608fffe44869cbc55f5fce2b
Lakshmi Addepalli <lakshi.ana@gmail.com>
2019-04-18 10:41:00 -0400

    Create intervalListIntersection.java

---
+++ Uber/intervalListIntersection.java
@@ -0,0 +1,24 @@
+class Solution {
+    public Interval[] intervalIntersection(Interval[] A, Interval[] B) {
+        List<Interval> ans = new ArrayList();
+        int i = 0, j = 0;
+
+        while (i < A.length && j < B.length) {
+            // Let's check if A[i] intersects B[j].
+            // lo - the startpoint of the intersection
+            // hi - the endpoint of the intersection
+            int lo = Math.max(A[i].start, B[j].start);
+            int hi = Math.min(A[i].end, B[j].end);
+            if (lo <= hi)
+                ans.add(new Interval(lo, hi));
+
+            // Remove the interval with the smallest endpoint
+            if (A[i].end < B[j].end)
+                i++;
+            else
+                j++;
+        }
+
+        return ans.toArray(new Interval[ans.size()]);
+    }
+}
